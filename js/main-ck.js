d882a512e96cc6c9af944baf13c82f5a6302247b
willdawsonme <mail@willdawson.me>
2015-02-15 15:26:19 +1100

    Lazy commit-all
    Sorry

---
+++ js/main-ck.js
@@ -0,0 +1,1 @@
+function me(){$(".btn-me").click(function(e){e.preventDefault();$(".page-wrap").css("position","fixed");$(".me").fadeIn()});$(".btn-close").click(function(e){e.preventDefault();$(".me").fadeOut(function(){$(".page-wrap").css("position","static")})})}$(document).ready(function(){me()});
